627feff69cbf45a9c640a64465390e9cef225f7e
Haris Shaikh <shaikh.m.haris@gmail.com>
2021-01-08 13:41:46 -0500

    completed app, ready to deploy

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-
-
 const app = {}
-app.displayPlayer = $('.player').html('Blue Turn')
+app.displayPlayer = $('.player').html(`Blue's Turn`).css("color", "blue")
 app.resetButton = $('.reset')
 app.gameCell = $.map($('.game-cell'), function(cell){
   return [cell]
@@ -14,6 +12,8 @@ app.cellHover = $(app.gameCell).hover(function () {
 app.gameCompleted = 
 app.currentPlayer = "X"
 app.gameGrid = ["", "", "", "", "", "", "", "", ""]
+app.randomButton = $('.random-mode').html('Random Mode: OFF')
+app.randomMode = false
 app.winConditions  = [
   [0, 1, 2],
   [3, 4, 5],
@@ -26,21 +26,27 @@ app.winConditions  = [
 ]
 app.gameWinner = $('.winner')
 
+
 $(document).ready(function () {
 
 app.handleReset = () => {
+
   app.currentPlayer = "X";
   app.gameGrid = ["", "", "", "", "", "", "", "", ""];
-  app.displayPlayer = $('.player').html('Blue Turn')
+  app.displayPlayer = $('.player').html(`Blue's Turn`).css("color", "blue")
   app.gameWinner = $('.winner').html('')
   $(app.gameCell).each(function () {
     $(this).removeClass("X")
     $(this).removeClass("O")
   });
   $('.game-complete').removeClass('show')
+  $(app.gameCell).attr('aria-pressed', 'false').attr('disabled', false)
+  $(app.randomButton).html('Random Mode: OFF')
+  app.randomMode = false
 }
 
 app.checkWin = (playerMove) => {
+
   return app.winConditions.some((combination) => {
     return combination.every(i => {
       return app.gameGrid[i] === playerMove
@@ -58,7 +64,7 @@ app.handleWin = (currentPlayer) => {
       $(app.gameWinner).html(`Blue Wins`)
       $('.game-complete').addClass('show')
       $(app.displayPlayer).html('')
-
+      $(app.gameCell).attr('aria-pressed', 'true').attr('disabled', true)
       return true
     }
   })()
@@ -68,6 +74,7 @@ app.handleWin = (currentPlayer) => {
       $(app.gameWinner).html(`Yellow Wins`)
       $('.game-complete').addClass('show')
       $(app.displayPlayer).html('')
+      $(app.gameCell).attr('aria-pressed', 'true').attr('disabled', true)     
       return true
     }
   })()
@@ -85,9 +92,29 @@ app.handleWin = (currentPlayer) => {
 }
 
 
+app.randomMove = (currentPlayer) => {
+
+  let randomIndex = Math.floor(Math.random() * app.gameGrid.length)
+  const conditionsArray = [
+    app.gameGrid[currentPlayer] !== "X" || app.gameGrid[currentPlayer] !== "O",
+    randomIndex === app.gameGrid.indexOf("X") || randomIndex === app.gameGrid.indexOf("O"),
+  ]
+
+  if (conditionsArray[0] === true && conditionsArray[1] === false) {
+    app.gameGrid[randomIndex] = app.currentPlayer
+    $(app.gameCell[randomIndex]).addClass(currentPlayer).attr('aria-pressed', 'true')
+  } else if (conditionsArray[0] === true && conditionsArray[1] === true) {
+    app.randomMove(currentPlayer)
+  }
+
+}
+
+
 app.handleGameCellClick = (e) => {
+
   const cellIndex = $(app.gameCell).index(e.target)
   const cellClicked = e.target
+
   if (app.gameGrid[cellIndex] !=="") {
     alert('This space is already played. Choose another move')
     return
@@ -98,21 +125,38 @@ app.handleGameCellClick = (e) => {
 
 app.playerMove = (cellIndex, cellClicked) => {
 
-  if (app.currentPlayer === "X") {
-    $(app.displayPlayer).html('Yellow Turn')
-    $(cellClicked).addClass("X");
+  const turnO = app.currentPlayer === "X" ? "O" : "X"
+  const turnX = app.currentPlayer === "X" ? "O" : "X"
+
+  if (app.currentPlayer === "X") { 
+    if (app.randomMode === true) {
+      app.randomMove(app.currentPlayer)
+      app.handleWin(app.currentPlayer)
+      app.currentPlayer = turnO
+      $(app.displayPlayer).html(`Yellow's Turn`).css("color", "yellow")
+    } 
+    else {  
+    $(app.displayPlayer).html(`Yellow's Turn`).css("color", "yellow")
+    $(cellClicked).addClass("X").attr('aria-pressed', 'true');
     app.gameGrid[cellIndex] = app.currentPlayer
     app.handleWin(app.currentPlayer)
-    app.currentPlayer = app.currentPlayer === "X" ? "O" : "X" ;
-
+    app.currentPlayer = turnO ;  
+  }
 
   } else if (app.currentPlayer === "O") {
-    $(cellClicked).addClass("O");
-    $(app.displayPlayer).html('Blue Turn')
+    if (app.randomMode === true) {
+      app.randomMove(app.currentPlayer)
+      app.handleWin(app.currentPlayer)
+      app.currentPlayer = turnX
+      $(app.displayPlayer).html(`Blue's Turn`).css("color", "blue")
+    } 
+    else {
+    $(cellClicked).addClass("O").attr('aria-pressed', 'true');
+    $(app.displayPlayer).html(`Blue's Turn`).css("color", "blue")
     app.gameGrid[cellIndex] = app.currentPlayer;
     app.handleWin(app.currentPlayer)
-    app.currentPlayer = app.currentPlayer === "O" ? "X" : "O";
-    
+    app.currentPlayer = turnX;
+    }
   }
 }
 
@@ -122,6 +166,15 @@ $(app.gameCell).each(function () {
   $(this).on('click', app.handleGameCellClick)
 });
 
-$('.share-facebook').on('click', app.handleFacebook)
+$(app.resetButton).on('click', app.handleReset)
+
+$(app.randomButton).click('click', function() {
+  app.randomMode = !app.randomMode;
+  if (app.randomMode === true) {
+    $(app.randomButton).html('Random Mode: ON').css("background-color", "green").css("color", "white")
+  } else if (app.randomMode === false) {
+    $(app.randomButton).html('Random Mode: OFF').css("background-color", "whitesmoke").css("color", "black")
+  }
+})
   
 }); 
